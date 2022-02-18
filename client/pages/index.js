477712db67a77fec734b228f8144baae23f87f3c
amitos80 <amitos80@gmail.com>
2022-02-18 23:55:48 +0200

    wip

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,10 +2,9 @@ import React, {useState, useEffect, useLayoutEffect} from 'react'
 import FluidAnimation from 'react-fluid-animation'
 import {ToastContainer, toast} from 'react-toastify'
 import { take, fill, sum, floor } from 'lodash'
+import config from '../../.config.json';
 const COMPRESSION_FACTOR = 8
-
-const VIEW_WIDTH = 1700
-const VIEW_HEIGHT = 1080;
+const { VIEW_WIDTH, VIEW_HEIGHT } = config
 
 const randomWhole = (min, max) => Math.floor(Math.random() * (max - min) + min)
 const randomReal = (min, max) => Math.random() * (max - min) + min
